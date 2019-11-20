54836cd1a6a662d5dd5d0283c002d2143d0aea80
pat-white-code <pat.k.white@gmail.com>
2019-11-19 21:06:30 -0600

    puts on branch checkers

--- 05week/checkers.js
+++ 05week/checkers.js
@@ -20,6 +20,10 @@ function Checker(color) {
 class Board {
   constructor() {
     this.grid = []
+
+    //SPEC 2.1 - In your Board class, create an attribute called this.checkers and assign it to an empty array. 
+    this.checkers = []
+
   }
   // method that creates an 8x8 array, filled with null values
   createGrid() {
@@ -56,8 +60,54 @@ class Board {
     }
     console.log(string);
   }
+  //Now create a method called this.createCheckers. In it, let's define our starting positions of the checkers on the grid.
 
-  // Your code here
+  createCheckers() {
+
+    //In it, let's define our starting positions of the checkers on the grid. In local variables, define whitePositions and blackPositions as array of [row, column] coordinates:
+
+    let whitePositions = [[0, 1], [0, 3], [0, 5], [0, 7],
+    [1, 0], [1, 2], [1, 4], [1, 6],
+    [2, 1], [2, 3], [2, 5], [2, 7]];
+
+    let blackPositions = [[5, 0], [5, 2], [5, 4], [5, 6],
+    [6, 1], [6, 3], [6, 5], [6, 7],
+    [7, 0], [7, 2], [7, 4], [7, 6]]
+
+    //In a for loop, iterate over the range from 0 - 11, with each index you want to:
+    for(let i = 0 ; i < 12 ; i++) {
+      //Instantiate a 'white' Checker
+      let checkerPiece = new Checker('white');
+      // Place that checker on the grid at the position corresponding with the index in the positions array
+      //i = [0,1]
+      let placeRow = whitePositions[i][0];
+      let placeCol = whitePositions[i][1];
+      this.grid[placeRow][placeCol] = checkerPiece;
+
+      // Push the checker into your this.checkers array
+      this.checkers.push(checkerPiece);
+      // Do all three steps above for a 'black' checker
+    };
+    for(let j = 0 ; j < 12 ; j++) {
+      //Instantiate a 'white' Checker
+      let checkerPiece = new Checker('black');
+      // Place that checker on the grid at the position corresponding with the index in the positions array
+      //i = [0,1]
+      let placeRow = blackPositions[i][0];
+      let placeCol = blackPositions[i][1];
+      this.grid[placeRow][placeCol] = checkerPiece;
+
+      // Push the checker into your this.checkers array
+      this.checkers.push(checkerPiece);
+      // Do all three steps above for a 'black' checker
+    };
+  }
+  }
+
+  // In a for loop, iterate over the range from 0 - 11, with each index you want to:
+
+
+
 }
 
 class Game {
