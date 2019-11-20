3e6d18026111320494f5c61d96ba2d67ab78d349
pat-white-code <pat.k.white@gmail.com>
2019-11-20 10:24:00 -0600

    doesnt pass final test, killchecker

--- 05week/checkers.js
+++ 05week/checkers.js
@@ -7,6 +7,9 @@ const rl = readline.createInterface({
   output: process.stdout
 });
 
+function difference(a, b) {
+  return Math.abs(a - b);
+}
 
 function Checker(color) {
   // Your code here
@@ -20,11 +23,12 @@ function Checker(color) {
 class Board {
   constructor() {
     this.grid = []
-
+  
     //SPEC 2.1 - In your Board class, create an attribute called this.checkers and assign it to an empty array. 
     this.checkers = []
 
-    this.selectChecker = this.selectChecker.bind(this)
+    this.selectChecker = this.selectChecker.bind(this);
+    this.killChecker = this.killChecker.bind(this);
   }
   // method that creates an 8x8 array, filled with null values
   createGrid() {
@@ -105,15 +109,24 @@ class Board {
   }
   //In your Board class, write a method this.selectChecker that takes two arguments row, column. All this does is return the checker at that particular spot on this.grid. This will be a handy "helper" function.
   selectChecker(row, col){
-    const checker = this.grid[row][col];
+    let checker = this.grid[row][col];
     return checker;
   }
 
   //TODO: THIS MAY HAVE TO BE BOUND TO THIS.
 
-}
+  //In your Board class, write a method killChecker that take a single argument position which is a coordinate pair, eg. [0, 5]. In it, use this.selectChecker to grab the checker at the position given. Find the index of that checker in the this.checkers array. then remove it by .splice()ing it out. Then assign the position on this.grid to null. That checker is dead.
 
-  // In a for loop, iterate over the range from 0 - 11, with each index you want to:
+  killChecker(pos){//[4, 1]
+    //pos = coordinate pair such as [0,5]. row = pos[0]. col = pos[1];
+    let killedChecker = this.selectChecker(pos[0], pos[1]);
+    //4, 1
+    killedIndex = this.checkers.indexOf(killedChecker);
+    this.checkers.splice(killedIndex, 1);
+    this.grid[pos[0], pos[1]] = null;
+  }
+
+}
 
 
 
@@ -129,14 +142,21 @@ class Game {
   }
   //Next, in your Game class, create a this.moveChecker method that takes two parameters start, end. These two arguments will each contain a row and a column, eg. 50, 41. Inside the method, use your board helper method selectChecker to select the checker at your starting rowcolumncoordinates and set it to a local variable checker. Then set that spot on the grid to null and set the spot at the end rowcolumn coordinate to the checker.
   moveChecker(start, end) {
-    //start is an array [50, 41] with 0 and 1. 0 = row, 1 = col
+    //start is a string with length of 2, such as 40, where row = string[0] and row =string[1]. 
     const checker = this.board.selectChecker(start[0], start[1]);
     //sets starting position to null (piece removed)
     this.board.grid[start[0]][start[1]] = null;
     //sets ending position to checker (piece placed)
     this.board.grid[end[0]][end[1]] = checker;
-
+    const distance = difference(start[0], end[0]);
+    if(distance > 1) {
+      let killRow = (start[0] + end[0]) / 2;
+      let killCol = (start[1] + end[1]) / 2;
+      this.board.killChecker([killRow, killCol]);//[4, 1]
+    }
   }
+
+  //In the Game class, in the moveChecker method, after you have moved the checker, check to see if the distance of the start row and the end row is 2 by finding the absolute value of the difference between the rows. If so, which means you must have jumped a checker, find the killPostition by finding the midpoint between the start and end positions. Then killChecker.
 }
 
 function getPrompt() {
